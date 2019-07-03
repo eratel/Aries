3df30ea2166cefafce0fd3c116d3589a095d52a4
sunjikai <sunjikai@hiynn.com>
2019-07-03 11:02:26 +0800

    调整架构

--- aries-example-server/src/main/java/com/abhsy/eureka/AriesExampleServerApplication.java
+++ aries-example-server/src/main/java/com/abhsy/eureka/AriesExampleServerApplication.java
@@ -1,17 +1,24 @@
 package com.abhsy.eureka;
 
+import com.abhsy.aries.constant.AriesConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringCloudApplication
-@EnableZuulProxy
+@RestController
 public class AriesExampleServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AriesExampleServerApplication.class, args);
     }
 
-
+    @RequestMapping("test")
+    public String test(@RequestHeader(AriesConstant.TOKENHEADER)String name){
+        return name;
+    }
 }
 
