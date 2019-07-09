0ce803a46f08f8603bd66b086d9ab50103937d0a
admin <425359983@qq.com>
2019-07-09 22:47:45 +0800

    调整项目架构

---
+++ version.bat
@@ -0,0 +1,19 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                     Ratel Aries                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Aries Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Ratel Aries
+@color 0a
+
+call mvn versions:set -DnewVersion=1.0.0
+pause
